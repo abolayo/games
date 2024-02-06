1534460614e9af3a2a45d5abe8ff68a16690e545
abolayo <medinatapampa@yahoo.com>
2024-02-06 16:59:29 -0400

    state game

--- us-states-game-start/scoreboard.py
+++ us-states-game-start/scoreboard.py
@@ -5,25 +5,22 @@ ALIGNMENT = "center"
 
 
 class Scoreboard(Turtle):
-    class Scoreboard(Turtle):
-        def __init__(self):
-            super().__init__()
-            self.score = 0
-            self.color('white')
-            self.penup()
-            self.hideturtle()
-            # self.goto(screen.title())
-            self.write_score()
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('black')
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 250)
+        # self.write_score()
 
-        def write_score(self):
-            self.clear()
-            self.write(f'Score: {self.score} , High Score: {self.high_score}', True, align=ALIGNMENT, font=FONT)
+    def write_score(self):
+        self.clear()
+        self.write(f'Score: {self.score}', True, align=ALIGNMENT, font=FONT)
 
-        def update(self):
-            self.score += 2
-            self.write_score()
+    def update(self):
+        self.score += 1
+        self.write_score()
 
-        def foul(self):
-            self.score -= 1
-            self.write_score()
+
 
