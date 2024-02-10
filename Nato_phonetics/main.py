c5e5d885a074fea1cd4e243a557d18d3b6f8c439
abolayo <medinatapampa@yahoo.com>
2024-02-10 10:48:25 -0400

    made into function

--- Nato_phonetics/main.py
+++ Nato_phonetics/main.py
@@ -3,17 +3,17 @@ import pandas as pd
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 game = True
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
-while game:
+
+
+def generative_phonetics():
     letter_word = list(input("Enter a word: ").upper())
-
-    if letter_word == list('X'):
-        print("Bye!!!")
-        game = False
+    try:
+        output = [nato_dict[letter] for letter in letter_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+        generative_phonetics()
     else:
-        try:
-            output = [nato_dict[letter] for letter in letter_word]
+        print(output)
 
-        except KeyError:
-            print("Sorry, only letters in the alphabet please!")
-        else:
-            print(output)
+
+generative_phonetics()
