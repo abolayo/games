ebf57e35374216658e8b7ce314c0ddedf2054f01
abolayo <medinatapampa@yahoo.com>
2024-02-10 10:38:33 -0400

    add try except block

--- Nato_phonetics/main.py
+++ Nato_phonetics/main.py
@@ -1,39 +1,18 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
 import pandas as pd
 
-student_data_frame = pd.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 df = pd.read_csv("nato_phonetic_alphabet.csv")
-# TODO 1. Create a dictionary in this format:
-
+game = True
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+while game:
+    letter_word = list(input("Enter a word: ").upper())
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-letter_word = list(input("Enter a word: ").upper())
+    if letter_word == list('X'):
+        print("Bye!!!")
+        game = False
+    else:
+        try:
+            output = [nato_dict[letter] for letter in letter_word]
+            print(output)
+        except KeyError:
+            print("Sorry, only letters in the alphabet please!")
 
-output = [nato_dict[letter] for letter in letter_word]
-print(output)
-
-# learning
-#
-# nato_words = [nato_dict[value] for (key, value) in nato_dict.items() if key in letter_word]
-# print(nato_words)
-# nato_word = [row.code for (index, row) in df.iterrows() if row.letter in letter_word]
-# print(nato_word)
