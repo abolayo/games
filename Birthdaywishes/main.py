4707a91ec5898ada77ed4edb44ec343ac8b5c365
abolayo <medinatapampa@yahoo.com>
2024-03-11 18:52:04 -0300

    modified

--- Birthdaywishes/main.py
+++ Birthdaywishes/main.py
@@ -1,16 +1,15 @@
 import pandas as pd
-PLACEHOLDER = '[name]'
+PLACEHOLDER = '[Name]'
 # Getting the names from the file
 
 df = pd.read_csv("./Input/Names/names.csv")
 names = df['name'].to_list()
+
 with open("./Input/Letters/starting_letter.txt", "r") as file:
     letters = file.read()
     for name in names:
-        name_clean = (name.strip())
-        new_letter = letters.replace(PLACEHOLDER, name_clean)
-    print(new_letter)
-    # # # # Save completed letter in the folder
-    #     with open(f"./ReadyToSend/{name}.txt", "w") as final_letter:
-    #
-    #         final_letter.writelines(new_letter)
+        new_letter = letters.replace(PLACEHOLDER, name)
+
+    # # # Save completed letter in the folder
+        with open(f"./ReadyToSend/{name}.txt", "w") as final_letter:
+            final_letter.writelines(new_letter)
