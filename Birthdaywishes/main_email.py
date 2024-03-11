71b8a5b76f1f7d24607b1f043eea7c60a30517b4
abolayo <medinatapampa@yahoo.com>
2024-03-11 18:41:37 -0300

    modified

--- Birthdaywishes/main_email.py
+++ Birthdaywishes/main_email.py
@@ -5,30 +5,26 @@ import datetime as dt
 my_email = "myemail@gmail.com"
 password = "get_password"  # from the sender email security settings
 
-
 # -------------------import data frame --------------------
 df = pd.read_csv("./Input/Names/names.csv")
 df['date'] = pd.to_datetime(df['date'])
+df['month'] = df.date.dt.month
+df['day'] = df.date.dt.day
+print(df.head())
+now = dt.datetime.now()
+now_month = now.month
+now_day = now.day
+print(now_month, now_day, now)
+for index, row in df.iterrows():
+    if now_month == row['month'] and now_day == row['day']:
+        with open(f'{row['name']}.txt') as birthday_person:
+            message = birthday_person.readlines()
 
-day = df.date.dt.day
-month = df.date.dt.month
-date = df.date
-name = df['name']
-email = df["email"]
-now = dt.datetime.now().month
-birthdate = "birthdate"
-print(day, month,date, now)
+        with smtplib.SMPT("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, passwd=password)
+            connection.sendmail(to_addrs=f"{row['email']}",
+                                from_addr=my_email,
+                                msg=f"Subject:Wishes\n\n{message}"
+                                )
 
-
-# if now - day == 0:
-#     with open(f'{name}.txt') as birthday_person:
-#         message = birthday_person.readlines()
-#
-#
-#     with smtplib.SMPT("smtp.gmail.com") as connection:
-#         connection.starttls()
-#         connection.login(user=my_email, passwd=password)
-#         connection.sendmail(to_addrs=f"{email}",
-#                             from_addr=my_email,
-#                             msg=f"Subject:Wishes\n\n{message}"
-#                             )
