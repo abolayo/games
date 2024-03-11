b23ecefd04ed192b589d297209161b183886f7c3
abolayo <medinatapampa@yahoo.com>
2024-03-11 16:15:12 -0300

    modified

--- Birthdaywishes/main.py
+++ Birthdaywishes/main.py
@@ -1,14 +1,20 @@
-PLACEHOLDER = "[name]"
+#PLACEHOLDER = "[name]"
 # # Getting the names from the file
-with open("Input/Names/names.txt", "r") as names:
-    names = names.readlines()
+# with open("Input/Names/names.csv", "r") as names:
+#     names = names.readlines()
+#
+# with open("./Input/Letters/starting_letter.txt", "r") as file:
+#     letters = file.read()
+#     for name in names:
+#         name = (name.strip())
+#         new_letter = letters.replace(PLACEHOLDER, name)
+#
+#     # # # Save completed letter in the folder
+#         with open(f"./ReadyToSend/{name}.txt", "w") as final_letter:
+#
+#             final_letter.writelines(new_letter)
+import pandas as pd
 
-with open("./Input/Letters/starting_letter.txt", "r") as file:
-    letters = file.read()
-    for name in names:
-        name = (name.strip())
-        new_letter = letters.replace(PLACEHOLDER, name)
-
-    # # # Save completed letter in the folder
-        with open(f"./ReadyToSend/{name}.txt", "w") as final_letter:
-            final_letter.writelines(new_letter)
+df = pd.read_csv("./Input/Names/names.csv")
+df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+print(df.head())
--- Birthdaywishes/main_email.py
+++ Birthdaywishes/main_email.py
@@ -1,22 +1,34 @@
 import smtplib
+import pandas as pd
 import datetime as dt
 
 my_email = "myemail@gmail.com"
-password = "get_password"  # from the sender emai security settings
-name = "placeholder"
-day = 0
-email = "email"
+password = "get_password"  # from the sender email security settings
 
-now = dt.datetime.now()
+
+# -------------------import data frame --------------------
+df = pd.read_csv("./Input/Names/names.csv")
+df['date'] = pd.to_datetime(df['date'])
+
+day = df.date.dt.day
+month = df.date.dt.month
+date = df.date
+name = df['name']
+email = df["email"]
+now = dt.datetime.now().month
 birthdate = "birthdate"
-if now - day == 0:
-    with open(f'{name}.txt') as birthday_person:
-        message = birthday_person.readlines()
+print(day, month,date, now)
 
-    with smtplib.SMPT("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_email, passwd=password)
-        connection.sendmail(to_addrs=f"{email}",
-                            from_addr=my_email,
-                            msg=f"Subject:Wishes\n\n{message}"
-                            )
+
+# if now - day == 0:
+#     with open(f'{name}.txt') as birthday_person:
+#         message = birthday_person.readlines()
+#
+#
+#     with smtplib.SMPT("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(user=my_email, passwd=password)
+#         connection.sendmail(to_addrs=f"{email}",
+#                             from_addr=my_email,
+#                             msg=f"Subject:Wishes\n\n{message}"
+#                             )
